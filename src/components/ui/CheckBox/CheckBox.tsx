927afdbd13c240abe60f5942684efa43ceb85b0b
thuyrubii <thuy.nguyen@digitalfortress.dev>
2023-05-09 18:39:55 +0700

    feat: implement checkbox component

---
+++ src/components/ui/CheckBox/CheckBox.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+interface IProp {
+  value: string | number;
+  className?: string;
+  classInput?: string;
+  classLabel?: string;
+  checked: boolean;
+  name?: string;
+  disabled?: boolean;
+  onChange?: (even: any) => void;
+  label?: string;
+  error?: string;
+}
+export default function CheckBox({
+  className,
+  classInput,
+  classLabel,
+  error,
+  label,
+  ...rest
+}: IProp) {
+  return (
+    <div>
+      <div className={`${className} flex items-center`}>
+        <input
+          {...rest}
+          type="checkbox"
+          className={`${classInput} h-4 w-4 rounded border-none bg-white text-[#0066FF] checked:bg-white dark:focus:ring-transparent`}
+        />
+        {label && (
+          <label
+            className={`${classLabel} ml-2 text-sm font-medium text-gray-900 dark:text-gray-300`}
+          >
+            {label}
+          </label>
+        )}
+      </div>
+      {error && (
+        <p className="mb-2 block text-sm font-medium text-red-800">{error}</p>
+      )}
+    </div>
+  );
+}
