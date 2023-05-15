0e621aa9b4d541b5f3c2f7adb5f506566a74e0e4
thuyrubii <thuy.nguyen@digitalfortress.dev>
2023-05-15 09:45:37 +0700

    fix: color button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -7,14 +7,16 @@ interface IProp extends React.ButtonHTMLAttributes<{}> {
   endIcon?: React.ReactElement;
   startIcon?: React.ReactElement;
   className: string;
+  color?: string;
 }
 export default function Button(props: IProp) {
-  const { className, children, startIcon, endIcon, ...rest } = props;
+  const { color, className, children, startIcon, endIcon, ...rest } = props;
   return (
     <button
       {...rest}
       className={clsx(
         className,
+        color,
         'flex items-center gap-2 rounded-lg px-4 py-2.5 text-center text-sm font-medium opacity-90',
         { 'bg-[#F2F4F7] text-[#D0D5DD] hover:bg-[#F2F4F7]': props.disabled },
       )}
